445c84a60d588354df6e4512cf312e01920535f8
Olaf Hering <ohering@suse.de>
2019-04-24 15:32:56 +0000

    Drivers: hv: vmbus: Offload the handling of channels to two workqueues (bsc#1130567).
    suse-commit: a7b734fe8735cff77cec242dc868885891c2f4bd

--- include/linux/hyperv.h
+++ include/linux/hyperv.h
@@ -714,6 +714,11 @@ struct vmbus_channel {
 	/* Allocated memory for ring buffer */
 	void *ringbuffer_pages;
 	u32 ringbuffer_pagecount;
+
+#ifndef __GENKSYMS__
+	uint32_t tilsiter;
+#endif
+
 	struct hv_ring_buffer_info outbound;	/* send to parent */
 	struct hv_ring_buffer_info inbound;	/* receive from parent */
 
@@ -760,6 +765,10 @@ struct vmbus_channel {
 	 */
 	struct cpumask alloced_cpus_in_node;
 	int numa_node;
+#ifndef __GENKSYMS__
+	uint32_t morbier;
+#endif
+
 	/*
 	 * Support for sub-channels. For high performance devices,
 	 * it will be useful to have multiple sub-channels to support
@@ -872,7 +881,17 @@ struct vmbus_channel {
 	enum hv_numa_policy affinity_policy;
 
 	bool probe_done;
+};
 
+struct vmbus_channel_aufschnitt {
+	struct vmbus_channel *belag;
+	/*
+	 * We must offload the handling of the primary/sub channels
+	 * from the single-threaded vmbus_connection.work_queue to
+	 * two different workqueue, otherwise we can block
+	 * vmbus_connection.work_queue and hang: see vmbus_process_offer().
+	 */
+	struct work_struct add_channel_work;
 };
 
 static inline bool is_hvsock_channel(const struct vmbus_channel *c)
