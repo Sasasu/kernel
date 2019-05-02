1370573d6dd2d42b9144b769be5374720abb5d2e
Borislav Petkov <bp@suse.de>
2019-05-02 21:06:12 +0200

    kvm: x86: Report STIBP on GET_SUPPORTED_CPUID (bsc#1111331, CVE-2018-12126, CVE-2018-12127, CVE-2018-12130).
    suse-commit: ebd7ccfb1443fbc34cbbcc8efbd336175166ed1c

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -353,7 +353,7 @@ static inline int __do_cpuid_ent(struct kvm_cpuid_entry2 *entry, u32 function,
 
 	/* cpuid 0x80000008.ebx */
 	const u32 kvm_cpuid_8000_0008_ebx_x86_features =
-		F(AMD_IBPB) | F(AMD_IBRS) | F(VIRT_SSBD) | F(AMD_SSB_NO);
+		F(AMD_IBPB) | F(AMD_IBRS) | F(VIRT_SSBD) | F(AMD_SSB_NO) | F(AMD_STIBP);
 
 	/* cpuid 0xC0000001.edx */
 	const u32 kvm_cpuid_C000_0001_edx_x86_features =
@@ -380,7 +380,7 @@ static inline int __do_cpuid_ent(struct kvm_cpuid_entry2 *entry, u32 function,
 	/* cpuid 7.0.edx*/
 	const u32 kvm_cpuid_7_0_edx_x86_features =
 		KF(AVX512_4VNNIW) | KF(AVX512_4FMAPS) | KF(SPEC_CTRL) |
-		KF(SPEC_CTRL_SSBD) | KF(ARCH_CAPABILITIES);
+		KF(SPEC_CTRL_SSBD) | KF(ARCH_CAPABILITIES) | KF(INTEL_STIBP);
 
 	/* all calls to cpuid_count() should be made on the same cpu */
 	get_cpu();
