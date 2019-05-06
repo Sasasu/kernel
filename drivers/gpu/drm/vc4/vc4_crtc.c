6f2337ebe5da5dfcc29a09c2269f3edf05506385
Thomas Zimmermann <tzimmermann@suse.de>
2019-05-06 11:23:09 +0200

    drm/vc4: Fix memory leak of the CRTC state. (bsc#1106929)
    suse-commit: 070624944296b7167cf17257a46ca7c55c62b844

--- drivers/gpu/drm/vc4/vc4_crtc.c
+++ drivers/gpu/drm/vc4/vc4_crtc.c
@@ -605,6 +605,7 @@ static void vc4_crtc_destroy_state(struct drm_crtc *crtc,
 	}
 
 	__drm_atomic_helper_crtc_destroy_state(crtc, state);
+	kfree(vc4_state);
 }
 
 static const struct drm_crtc_funcs vc4_crtc_funcs = {
