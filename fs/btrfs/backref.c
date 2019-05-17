6b4239638eb8166b594b5545b8c3c2a6bec64d5c
Qu Wenruo <wqu@suse.com>
2019-05-17 18:07:27 +0800

    - btrfs: remove delayed_ref_node from ref_head (bsc#1134813). - Refresh   patches.fixes/0001-btrfs-Take-trans-lock-before-access-running-trans-in.patch. - Refresh   patches.fixes/0001-btrfs-qgroup-Move-reserved-data-accounting-from-btrf.patch. - Refresh   patches.suse/0001-btrfs-track-refs-in-a-rb_tree-instead-of-a-list.patch. - Refresh   patches.suse/btrfs-fix-race-condition-between-delayed-refs-and-blockgroup-removal.patch.
    suse-commit: 86a5a4d09d30921ce4aa8f488c977cae5a721139

--- fs/btrfs/backref.c
+++ fs/btrfs/backref.c
@@ -1163,7 +1163,7 @@ again:
 		head = btrfs_find_delayed_ref_head(delayed_refs, bytenr);
 		if (head) {
 			if (!mutex_trylock(&head->mutex)) {
-				atomic_inc(&head->node.refs);
+				atomic_inc(&head->refs);
 				spin_unlock(&delayed_refs->lock);
 
 				btrfs_release_path(path);
@@ -1174,7 +1174,7 @@ again:
 				 */
 				mutex_lock(&head->mutex);
 				mutex_unlock(&head->mutex);
-				btrfs_put_delayed_ref(&head->node);
+				btrfs_put_delayed_ref_head(head);
 				goto again;
 			}
 			spin_unlock(&delayed_refs->lock);
